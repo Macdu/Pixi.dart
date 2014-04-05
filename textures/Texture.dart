0a43f1947f164bafeb29912718226b3773e1630b
macdu <dufay.marc@gmail.com>
2014-04-05 17:35:33 +0200

    Mise en page du fichier Texture.dart

--- textures/Texture.dart
+++ textures/Texture.dart
@@ -12,55 +12,54 @@ int TextureCacheIdGenerator = 0;
  * A texture stores the information that represents an image or part of an image. It cannot be added
  * to the display list directly. To do this use PIXI.Sprite. If no frame is provided then the whole image is used
  */
-class Texture extends EventTarget{
-  
+class Texture extends EventTarget {
+
   bool noFrame = false;
-  
+
   /**
        * The base texture of that this texture uses
        *
        * @property baseTexture
        * @type BaseTexture
        */
-      BaseTexture baseTexture;
+  BaseTexture baseTexture;
 
-      /**
+  /**
        * The frame specifies the region of the base texture that this texture uses
        *
        * @property frame
        * @type Rectangle
        */
-      Rectangle frame;
+  Rectangle frame;
 
-      /**
+  /**
        * The trim point
        *
        * @property trim
        * @type Rectangle
        */
-      Rectangle trim = null;
-      
-      int width;
-      
-      int height;
-      
-      bool updateFrame = false;
-      
-      TextureUvs _uvs;
-  
-/**
+  Rectangle trim = null;
+
+  int width;
+
+  int height;
+
+  bool updateFrame = false;
+
+  TextureUvs _uvs;
+
+  /**
  * @class Texture
  * @uses EventTarget
  * @constructor
  * @param baseTexture {BaseTexture} The base texture source to create the texture from
  * @param frame {Rectangle} The rectangle frame of the texture to show
  */
-Texture(BaseTexture baseTexture,[Rectangle frame = null])
-{
-  this._load(baseTexture,frame);
-}
+  Texture(BaseTexture baseTexture, [Rectangle frame = null]) {
+    this._load(baseTexture, frame);
+  }
 
-/**
+  /**
  * Helper function that returns a texture based on an image url
  * If the image is not in the texture cache it will be  created and loaded
  *
@@ -71,20 +70,18 @@ Texture(BaseTexture baseTexture,[Rectangle frame = null])
  * @param scaleMode {Number} Should be one of the PIXI.scaleMode consts
  * @return Texture
  */
-Texture.fromImage(String imageUrl,[bool crossorigin = false,int scaleMode = 0])
-{
+  Texture.fromImage(String imageUrl, [bool crossorigin = false, int scaleMode = 0]) {
     Texture texture = TextureCache[imageUrl];
 
-    if(texture != null)
-    {
-        this._load(new BaseTexture.fromImage(imageUrl, crossorigin, scaleMode));
-        TextureCache[imageUrl] = texture;
-    }else this._load(texture.baseTexture, texture.frame);
+    if (texture != null) {
+      this._load(new BaseTexture.fromImage(imageUrl, crossorigin, scaleMode));
+      TextureCache[imageUrl] = texture;
+    } else this._load(texture.baseTexture, texture.frame);
 
-}
+  }
 
 
-/**
+  /**
  * Helper function that returns a texture based on a frame id
  * If the frame id is not in the texture cache an error will be thrown
  *
@@ -93,16 +90,15 @@ Texture.fromImage(String imageUrl,[bool crossorigin = false,int scaleMode = 0])
  * @param frameId {String} The frame id of the texture
  * @return Texture
  */
-Texture.fromFrame(String frameId)
-{
+  Texture.fromFrame(String frameId) {
     Texture texture = TextureCache[frameId];
-    if(texture == null) throw new Exception('The frameId "' + frameId + '" does not exist in the texture cache ');
-    this._load(texture.baseTexture,texture.frame);
-}
+    if (texture == null) throw new Exception('The frameId "' + frameId + '" does not exist in the texture cache ');
+    this._load(texture.baseTexture, texture.frame);
+  }
 
 
 
-/**
+  /**
  * Helper function that returns a texture based on a canvas element
  * If the canvas is not in the texture cache it will be  created and loaded
  *
@@ -112,97 +108,91 @@ Texture.fromFrame(String frameId)
  * @param scaleMode {Number} Should be one of the PIXI.scaleMode consts
  * @return Texture
  */
-Texture.fromCanvas(CanvasElement canvas,[ scaleMode = 0])
-{
+  Texture.fromCanvas(CanvasElement canvas, [scaleMode = 0]) {
     BaseTexture baseTexture = new BaseTexture.fromCanvas(canvas, scaleMode);
 
     this._load(baseTexture);
 
-}
+  }
 
 
 
-void _load(BaseTexture baseTexture,[Rectangle frame = null]){
-  
-  if(frame != null)
-      {
-          this.noFrame = true;
-          frame = new Rectangle(0.0,0.0,1.0,1.0);
-      }
-      /**
+  void _load(BaseTexture baseTexture, [Rectangle frame = null]) {
+
+    if (frame != null) {
+      this.noFrame = true;
+      frame = new Rectangle(0.0, 0.0, 1.0, 1.0);
+    }
+    /**
        * The base texture of that this texture uses
        *
        * @property baseTexture
        * @type BaseTexture
        */
-      this.baseTexture = baseTexture;
+    this.baseTexture = baseTexture;
 
-      /**
+    /**
        * The frame specifies the region of the base texture that this texture uses
        *
        * @property frame
        * @type Rectangle
        */
-      this.frame = frame;
+    this.frame = frame;
 
-      if(baseTexture.hasLoaded)
-      {
-          if(this.noFrame)frame = new Rectangle(0.0,0.0, baseTexture.width.toDouble(), baseTexture.height.toDouble());
-        
-          this.setFrame(frame);
-      }
-      else
-      {
-          baseTexture.listen('loaded', this.onBaseTextureLoaded);
-      }
-  
-}
+    if (baseTexture.hasLoaded) {
+      if (this.noFrame) frame = new Rectangle(0.0, 0.0, baseTexture.width.toDouble(), baseTexture.height.toDouble());
+
+      this.setFrame(frame);
+    } else {
+      baseTexture.listen('loaded', this.onBaseTextureLoaded);
+    }
+
+  }
 
 
-/**
+  /**
  * Called when the base texture is loaded
  *
  * @method onBaseTextureLoaded
  * @param event
  * @private
  */
-void onBaseTextureLoaded([_])
-{
+  void onBaseTextureLoaded([_]) {
     BaseTexture baseTexture = this.baseTexture;
 
-    if(this.noFrame)this.frame = new Rectangle(0.0,0.0, baseTexture.width.toDouble(), baseTexture.height.toDouble());
-    
+    if (this.noFrame) this.frame = new Rectangle(0.0, 0.0, baseTexture.width.toDouble(), baseTexture.height.toDouble());
+
     this.setFrame(this.frame);
 
-    this.fire( { 'type': 'update', 'content': this } );
-}
+    this.fire({
+      'type': 'update',
+      'content': this
+    });
+  }
 
-/**
+  /**
  * Destroys this texture
  *
  * @method destroy
  * @param destroyBase {Boolean} Whether to destroy the base texture as well
  */
-void destroy([bool destroyBase = false])
-{
-    if(destroyBase) this.baseTexture.destroy();
-}
+  void destroy([bool destroyBase = false]) {
+    if (destroyBase) this.baseTexture.destroy();
+  }
 
-/**
+  /**
  * Specifies the rectangle region of the baseTexture
  *
  * @method setFrame
  * @param frame {Rectangle} The frame of the texture to set it to
  */
-void setFrame(Rectangle frame)
-{
+  void setFrame(Rectangle frame) {
     this.frame = frame;
     this.width = frame.width.toInt();
     this.height = frame.height.toInt();
 
-    if(frame.x + frame.width > this.baseTexture.width || frame.y + frame.height > this.baseTexture.height)
-    {
-        throw new Exception('Texture Error: frame does not fit inside the base Texture dimensions ' + this.toString());
+    if (frame.x + frame.width > this.baseTexture.width || frame.y + frame.height > this.baseTexture.height) {
+      throw new Exception('Texture Error: frame does not fit inside the base Texture dimensions ' + this.toString());
     }
 
     this.updateFrame = true;
@@ -211,11 +201,10 @@ void setFrame(Rectangle frame)
 
 
     //this.dispatchEvent( { type: 'update', content: this } );
-}
+  }
 
-void _updateWebGLuvs()
-{
-    if(this._uvs == null)this._uvs = new TextureUvs();
+  void _updateWebGLuvs() {
+    if (this._uvs == null) this._uvs = new TextureUvs();
 
     Rectangle frame = this.frame;
     int tw = this.baseTexture.width;
@@ -232,10 +221,10 @@ void _updateWebGLuvs()
 
     this._uvs.x3 = frame.x / tw;
     this._uvs.y3 = (frame.y + frame.height) / th;
-}
+  }
 
 
-/**
+  /**
  * Adds a texture to the textureCache.
  *
  * @static
@@ -243,12 +232,11 @@ void _updateWebGLuvs()
  * @param texture {Texture}
  * @param id {String} the id that the texture will be stored against.
  */
-static void addTextureToCache(Texture texture,String id)
-{
+  static void addTextureToCache(Texture texture, String id) {
     TextureCache[id] = texture;
-}
+  }
 
-/**
+  /**
  * Remove a texture from the textureCache.
  *
  * @static
@@ -256,33 +244,30 @@ static void addTextureToCache(Texture texture,String id)
  * @param id {String} the id of the texture to be removed
  * @return {Texture} the texture that was removed
  */
-static Texture removeTextureFromCache(id)
-{
+  static Texture removeTextureFromCache(id) {
     Texture texture = TextureCache[id];
     TextureCache[id] = null;
     return texture;
-}
+  }
 
-// this is more for webGL.. it contains updated frames..
-static List frameUpdates = [];
-
-}
-
-class TextureUvs
-{
-    double x0 = 0.0;
-    double y0 = 0.0;
-
-    double x1 = 0.0;
-    double y1 = 0.0;
-
-    double x2 = 0.0;
-    double y2 = 0.0;
-
-    double x3 = 0.0;
-    double y4 = 0.0;
-
+  // this is more for webGL.. it contains updated frames..
+  static List frameUpdates = [];
 
 }
 
+class TextureUvs {
+  double x0 = 0.0;
+  double y0 = 0.0;
+
+  double x1 = 0.0;
+  double y1 = 0.0;
+
+  double x2 = 0.0;
+  double y2 = 0.0;
+
+  double x3 = 0.0;
+  double y4 = 0.0;
+
+
+}
 
